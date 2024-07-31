a642ed2c863a7178eef9162f522859c8b70bb83d
niloofar souri <nlfrsouri@gmail.com>
2024-07-31 15:27:13 +0330

    Sign-in

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,24 @@ function App() {
 
         <div className='right_section'>
 
-          <div className='right_section_sub'>
+          <div className='right_section_sub_one'>
             <img src="./icons8-add-user-male-50.png" alt="persona" />
             <h1>Sign in</h1>
             <input type="text" placeholder='Name' />
             <input type="text" placeholder='Family' />
-            <div className='checkbox_box'> 
-            <input type="checkbox" />
-            <span>Remmember me</span>
+
+            <div className='checkbox_box'>
+              <input type="checkbox" />
+              <span>Remmember me</span>
             </div>
+            
+
             <Button variant="contained">Sign in</Button>
+          </div>
+
+          <div className='right_section_sub_two'>
+            <a href="#">Forgot password?</a>
+            <a href="#">Don't have an account? Sign Up</a>
           </div>
 
         </div>
